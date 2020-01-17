435f43431e49c29a0b363faf21bc6d851b2fbcb9
SBA <sba@kabylake.Speedport_W_724V_Typ_A_05011603_06_001>
2020-01-18 00:48:39 +0100

    added gitignore removing docker files before build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . config.sh
-
+rm -rf docker
 mkdir docker
 $DKR build ./ -t $PROJ
 $DKR run -it --rm -v ./docker:/go/src/docker --privileged=true --net=host localhost/$PROJ /bin/bash cnt_build.sh
